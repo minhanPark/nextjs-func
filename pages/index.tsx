69e7f9d4725a227ae9a271c064d6e0230c695c6e
박민한 <alsgks1218@naver.com>
2022-04-29 17:41:29 +0900

    ssr 및 csr 시 소스코드 확인

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,12 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import dynamic from "next/dynamic";
 import Link from "next/link";
+import { useState } from "react";
 
 const Temp = dynamic(() => import("../components/temp"), { ssr: false });
 
 const Home: NextPage = () => {
+  const [count, setCount] = useState(0);
   return (
     <div>
       <Head>
@@ -18,6 +20,11 @@ const Home: NextPage = () => {
       <Link href="/bye">
         <a>바이 페이지</a>
       </Link>
+      <div>
+        <h2>count</h2>
+        <p>{count}</p>
+        <button onClick={() => setCount((prev) => prev + 1)}>올리기</button>
+      </div>
     </div>
   );
 };
